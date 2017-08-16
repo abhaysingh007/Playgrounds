6d9f6648d0a397aa76c6e58d77df545e73adf58d
abhay singh <abhay.singh@exilant.com>
2017-08-16 10:03:08 +0530

    First commit

---
+++ MyPlayground.playground/Pages/Retain Count.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Cook{
+    let name: String = "Abhay"
+//    let cat: String = "Veg"
+}
+print(CFGetRetainCount)
+let c1 = Cook()
+print(CFGetRetainCount(c1))
+let c2 = c1
+print(CFGetRetainCount(c2))
+let c3 = c2
+print(CFGetRetainCount(c3))
+//print(c1.cat, c1.name)
+////c2.name = "Abhisek"
+//print(c2.cat, c2.name)
+//print(c3.cat, c3.name)
+
+
